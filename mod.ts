eb4e05759f51064d555a9101b3bc01fb345e9689
ryosukehagiwara <hukurahagi1109@gmail.com>
2021-02-04 07:18:56 +0900

    add read csv file function

--- mod.ts
+++ mod.ts
@@ -1,9 +1,25 @@
 import { join } from 'https://deno.land/std/path/mod.ts';
+import { BufReader } from "https://deno.land/std/io/bufio.ts";
+import { parse } from 'https://deno.land/std/encoding/csv.ts';
 
-async function readFile() {
+async function readTextFile() {
   const path = join("text_files","hello.txt")
-  const data = await Deno.readTextFile(path)
-  console.log(data)
+  const textData = await Deno.readTextFile(path)
+  console.log(textData)
 }
 
-await readFile()
+async function readCSVFile() {
+  const path = join("csv_files","kepler_exoplanets_nasa.csv")
+  const csvData = await Deno.open(path)
+  const bufReader = new BufReader(csvData)
+  const result = await parse(bufReader,{
+    skipFirstRow: true,
+    comment: "#"
+  })
+  Deno.close(csvData.rid)
+
+  console.log(result)
+}
+
+await readTextFile()
+await readCSVFile()
