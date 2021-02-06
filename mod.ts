29b62b22f2acc0d550531158db35427cbfa7ef53
ryosukehagiwara <hukurahagi1109@gmail.com>
2021-02-06 16:48:45 +0900

    add find habitable planets function

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@ async function readTextFile() {
   const textData = await Deno.readTextFile(path)
   console.log(textData)
 }
+// await readTextFile()
 
 async function readCSVFile() {
   const path = join("csv_files","kepler_exoplanets_nasa.csv")
@@ -20,6 +21,35 @@ async function readCSVFile() {
 
   console.log(result)
 }
+// await readCSVFile()
 
-await readTextFile()
-await readCSVFile()
+interface Planet {
+  [key:string]: string
+}
+
+async function findHabitablePanets() {
+  const path = join("csv_files","kepler_exoplanets_nasa.csv")
+  const csvData = await Deno.open(path)
+  const bufReader = new BufReader(csvData)
+  const result = await parse(bufReader,{
+    skipFirstRow: true,
+    comment: "#"
+  })
+  Deno.close(csvData.rid)
+
+  const planets = (result as Array<Planet>).filter((planet)=>{
+    const planetaryRadius = Number(planet["koi_prad"])
+    const stellerMass = Number(planet["koi_smass"])
+    const stellerRadius = Number(planet["koi_srad"])
+
+    return planet["koi_disposition"] === "CONFIRMED" 
+    && planetaryRadius > 0.5 && planetaryRadius < 1.5
+    && stellerMass > 0.78 && stellerMass > 1.04
+    && stellerRadius > 0.99 && stellerRadius > 1.01
+  }) 
+
+  return planets
+}
+
+const result = await findHabitablePanets()
+console.log(`${result.length} habitable planets founded!`)
